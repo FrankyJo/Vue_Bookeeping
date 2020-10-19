b8e26a2fdf1311902420c7d0e117420c36565e84
Denis Popov <popovdenys.web@gmail.com>
2020-10-19 15:04:19 +0300

    create project. setting up routing. realize trigger in navbar and dynamic date.

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="grey darken-1 empty-layout">
+        asdasdasdasd
+        <router-view />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'EmptyLayout'
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,67 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/login',
+        name: 'Login',
+        meta: {layout: 'empty'},
+        component: () => import('../views/Login')
+    },
+    {
+        path: '/register',
+        name: 'Login',
+        meta: {layout: 'empty'},
+        component: () => import('../views/Register')
+    },
+    {
+        path: '/categories',
+        name: 'Categories',
+        meta: {layout: 'main'},
+        component: () => import('../views/Categories.vue')
+    },
+    {
+        path: '/detail',
+        name: 'Detail',
+        meta: {layout: 'main'},
+        component: () => import('../views/Detail.vue')
+    },
+    {
+        path: '/history',
+        name: 'History',
+        meta: {layout: 'main'},
+        component: () => import('../views/History.vue')
+    },
+    {
+        path: '/',
+        name: 'Home',
+        meta: {layout: 'main'},
+        component: () => import('../views/Home.vue')
+    },
+    {
+        path: '/planning',
+        name: 'Planning',
+        meta: {layout: 'main'},
+        component: () => import('../views/Planning.vue')
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        meta: {layout: 'main'},
+        component: () => import('../views/Profile.vue')
+    },
+    {
+        path: '/record',
+        name: 'Record',
+        meta: {layout: 'main'},
+        component: () => import('../views/Record.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
