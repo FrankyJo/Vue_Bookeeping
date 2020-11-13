996bebefaf594c049656bc3cda09f6c5a5f53659
Denis Popov <popovdenys.web@gmail.com>
2020-11-13 18:37:41 +0200

    build project

---
+++ dist/precache-manifest.0bbd09c4d85962fbfc7bf7bd1182fbe7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922fa9f76434a9e56dc5",
+    "url": "/css/app.a931a63d.css"
+  },
+  {
+    "revision": "0664f2dee8e88afc9823",
+    "url": "/css/chunk-79d24435.08c7e127.css"
+  },
+  {
+    "revision": "c235b18947a453a8d413",
+    "url": "/css/chunk-7f422789.18230414.css"
+  },
+  {
+    "revision": "52c5d21e46cefeeac5c9",
+    "url": "/css/chunk-c5fa7608.bf044e7e.css"
+  },
+  {
+    "revision": "89eb1364a37eb5fe61a3",
+    "url": "/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "da629e6afe18927c31355af27838f90b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "922fa9f76434a9e56dc5",
+    "url": "/js/app.ac8dc081.js"
+  },
+  {
+    "revision": "65bc181ff3c3d550eff8",
+    "url": "/js/chunk-10b86dd9.4cb11a0b.js"
+  },
+  {
+    "revision": "a05ee6e7fb35a4bd74f8",
+    "url": "/js/chunk-2d217a99.ae2a99ce.js"
+  },
+  {
+    "revision": "971383cb011e0f4e4790",
+    "url": "/js/chunk-53c8ec65.040a2826.js"
+  },
+  {
+    "revision": "0347b9e697def4c7c2da",
+    "url": "/js/chunk-5f4ad635.9565f4f0.js"
+  },
+  {
+    "revision": "0ad18443a20ec2524b88",
+    "url": "/js/chunk-72449522.3a88b625.js"
+  },
+  {
+    "revision": "0664f2dee8e88afc9823",
+    "url": "/js/chunk-79d24435.ec57f2e0.js"
+  },
+  {
+    "revision": "c235b18947a453a8d413",
+    "url": "/js/chunk-7f422789.8bb6932d.js"
+  },
+  {
+    "revision": "7c67ce2137c8949baafa",
+    "url": "/js/chunk-c420df12.6f524259.js"
+  },
+  {
+    "revision": "52c5d21e46cefeeac5c9",
+    "url": "/js/chunk-c5fa7608.9147f577.js"
+  },
+  {
+    "revision": "89eb1364a37eb5fe61a3",
+    "url": "/js/chunk-vendors.a37d496b.js"
+  },
+  {
+    "revision": "abb12be3e40488d68858d9971b5097cf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
